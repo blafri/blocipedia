b3e75993900601ab213ea0849be86d7d40081fc5
Blayne Farinha <Blayne.farinha@gmail.com>
2015-05-19 12:22:45 -0400

    Moved reset password and signout feature specs to their own files

---
+++ spec/features/user_reset_password_spec.rb
@@ -0,0 +1,28 @@
+feature "Reset Password" do
+  let!(:user) { create(:user, email: 'rich.froning@example.com', password: 'Abcd1234') }
+  
+  before do
+    ActionMailer::Base.deliveries = []
+    visit new_user_password_path
+  end
+  
+  context "is successful" do
+    scenario "when an existing user email is entered" do
+      fill_in 'user_email', with: user.email
+      click_button 'Send me reset password instructions'
+      
+      expect(page).to have_content('You will receive an email with instructions on how to reset your password in a few minutes')
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+  end
+  
+  context "is unsuccessful" do
+    scenario "when an email is entered that does not match any existing users" do
+      fill_in 'user_email', with: 'test@test.com'
+      click_button 'Send me reset password instructions'
+      
+      expect(page).to have_content('Email not found')
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
+    end
+  end
+end

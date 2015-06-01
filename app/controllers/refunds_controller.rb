9a62f7cc95d62bfb6b7a415447654668b27ab01f
Blayne Farinha <Blayne.farinha@gmail.com>
2015-06-01 08:53:50 -0400

    Refactored rufunds_controller.rb to remove redundancy

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -2,19 +2,17 @@ class RefundsController < ApplicationController
   def create
     if current_user.role != 'premium'
       flash[:error] = 'Your account is already a standard account'
-      redirect_to edit_user_registration_path
-    end
-    
-    sale = Blocipedia::PaypalPayment.new(sale_id: current_user.paypal_sale_id)
-    sale.find_sale
-    
-    if sale.refund_payment
-      current_user.update_attributes(role: 'standard', paypal_sale_id: nil)
-      flash[:notice] = 'Your account has been successfully downgraded and your money refunded.'
-      redirect_to edit_user_registration_path
     else
-      flash[:error] = 'There was a problem downgrading your account. Please try again.'
-      redirect_to edit_user_registration_path
+      sale = Blocipedia::PaypalPayment.new(sale_id: current_user.paypal_sale_id)
+      sale.find_sale
+      
+      if sale.refund_payment
+        current_user.update_attributes(role: 'standard', paypal_sale_id: nil)
+        flash[:notice] = 'Your account has been successfully downgraded and your money refunded.'
+      else
+        flash[:error] = 'There was a problem downgrading your account. Please try again.'
+      end
     end
+    redirect_to edit_user_registration_path
   end
 end
